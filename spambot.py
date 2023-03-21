6111f1ca66bf4bf662b6cc555b481ac03bd2e758
Tucunare <112496094+S4turny@users.noreply.github.com>
2023-03-21 15:20:23 -0300

    Add files via upload

--- spambot.py
+++ spambot.py
@@ -1,40 +1,50 @@
-#####CODE BY TUCUNAS
+#####CODE BY TUCUNAS --- 2023
 
 from time import sleep 
 import pyautogui as auto
 from tkinter import *
 from tkinter import messagebox
+import webbrowser as web
+import random
 
+#############################################
+                                            
 windown = Tk()
 menu = Menu(windown)
 windown.config(menu = menu)
-global msg_c
-global msg_q
+
+#############################################
 
 def spam():
-    sleep(5)
+    sleep(3)
     global info1
     info1 = Entry1.get()
     global info2
-    info2 = Entry2.get()
-    isInt = isinstance(info2, int)
+    try:
+        info2 = int(Entry2.get())
+    except ValueError:
+        messagebox.showerror('ERRO','Digite um valor valido!')
+        return
     if info1 == '':
         messagebox.showerror('ERRO','Digite um valor valido!' )
-    if isInt == False:
-        messagebox.showerror('ERRO', 'Digite um valor valido!')
+        return
     auto.keyDown('alt')
     auto.press('tab')
     auto.keyUp('alt')
     for i in range(int(info2)):
         auto.write(info1)
         auto.press('enter')
-    
+        
+#############################################
+        
+def github():
+    web.open('https://github.com/S4turny/SpamBot.py/tree/main')    
+
+##########DESIGN DA JANELA##########
 
 windown.title('SpamBot by Tucunare')
 filemenu = Menu(menu)
 menu.add_cascade(label = 'Opções', menu = filemenu)
-filemenu.add_command(label = 'Contato')
-filemenu.add_separator()
 filemenu.add_command(label = 'Sair', command = windown.quit)
 Label(windown, text = 'Digite a mensagem e a quantidade de envios').grid(padx = 15, pady = 15)
 Label(windown, text = 'Mensagem').grid(padx = 20)
@@ -43,7 +53,11 @@ Entry1.grid()
 Label(windown, text = 'Quantidade').grid(padx = 20)
 Entry2 = Entry(windown, width = 25)
 Entry2.grid()
-btn_save = Button(windown,text = 'Spam', command=spam)
+btn_save = Button(windown,text = 'Spam', command = spam)
 btn_save.grid(pady = 10)
-Label(windown, text = 'O programa da alt + tab então deixe o chat em segundo plano!').grid(pady = 11)
+Button(windown, text='MINHA GITHUB',command = github).grid(padx = 10, pady = 8)
+Label(windown, text = 'deixe o chat em segundo plano!').grid(pady = 11)
+
+#############################################
+
 windown.mainloop()
